b6d1bd70036febd9d623eeb505475c7bcf3262b4
CookieHCl <nhc7502@snu.ac.kr>
2025-11-20 22:02:27 +0900

    Implement sync rush branch

--- src/lib.rs
+++ src/lib.rs
@@ -1,5 +1,40 @@
-pub fn add(left: u64, right: u64) -> u64 {
-    left + right
+use std::sync::mpsc;
+use std::thread;
+
+pub type Job<T> = Box<dyn FnOnce() -> T + Send + 'static>;
+pub type JobResult<T> = thread::Result<T>;
+
+pub fn sync<T: Send + 'static>(jobs: Vec<Job<T>>) -> Vec<JobResult<T>> {
+    let handles: Vec<_> = jobs
+        .into_iter()
+        .map(|job| thread::spawn(|| job()))
+        .collect();
+
+    handles
+        .into_iter()
+        .map(|h| h.join())
+        .collect::<Vec<JobResult<T>>>()
+}
+
+pub fn rush<T: Send + 'static>(jobs: Vec<Job<T>>) -> (usize, T) {
+    let (tx, rx) = mpsc::channel::<(usize, T)>();
+
+    for (i, job) in jobs.into_iter().enumerate() {
+        let tx = tx.clone();
+        thread::spawn(move || {
+            let out = job();
+            let _ = tx.send((i, out)); // can return Err when rush is finished
+        });
+    }
+
+    // Return on first exit, other threads will be continued
+    rx.recv().expect("at least one job is required for rush()")
+}
+
+pub fn branch<T: Send + 'static>(jobs: Vec<Job<T>>) -> Vec<thread::JoinHandle<T>> {
+    jobs.into_iter()
+        .map(|job| thread::spawn(|| job()))
+        .collect()
 }
 
 #[cfg(test)]
@@ -7,8 +42,5 @@ mod tests {
     use super::*;
 
     #[test]
-    fn it_works() {
-        let result = add(2, 2);
-        assert_eq!(result, 4);
-    }
+    fn it_works() {}
 }
